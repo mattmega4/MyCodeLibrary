ad3f78387803fa4d13e2405a24928fcc9c48eb66
mattmega4 <matthewhsingleton@Matthews-MacBook-Pro.local>
2017-04-19 16:53:15 -0700

    Guard vs if let

---
+++ MyCodeLibrary/Functions.swift
@@ -0,0 +1,29 @@
+//
+//  Functions.swift
+//  MyCodeLibrary
+//
+//  Created by Matthew Singleton on 4/18/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+// MARK: External vs Internal Parametsrs
+
+// Example:
+
+//func drawHorizontalLine(from startX: Double, to endX: Double, using color: UIColor) {
+//  //
+//}
+
+// 'from, to, using' are external
+// 'startX, endX, color' are internal
+
+// Internal are used internally, as in insie the metho
+// External are use when called. For the purpose of reading like english when called
+
+// Usage
+
+//let temp = drawHorizontalLine(from: 5.0, to: 8.5, using: .blue)
---
+++ MyCodeLibrary/Guard vs If Let.swift
@@ -0,0 +1,58 @@
+//
+//  Guard vs If Let.swift
+//  MyCodeLibrary
+//
+//  Created by Matthew Howes Singleton on 4/19/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import Foundation
+
+// Guard vs If Let with Optionals
+
+// MARK: If Let 
+
+
+//func fooBinding(x: Int?) {
+//    if let x = x where x > 0 {
+//        // Do stuff with x
+//        x.description
+//    }
+//    
+//    // Value requirements not met, do something
+//}
+
+
+// MARK: Guard
+
+
+//func fooGuard(x: Int?) {
+//    guard let x = x where x > 0 else {
+//        // Value requirements not met, do something
+//        return
+//    }
+//    
+//    // Do stuff with x
+//    x.description
+//}
+
+
+// MARK: Guard vs If Let without Optionals
+
+//func fooNonOptionalGood(x: Int) {
+//    guard x > 0 else {
+//        // Value requirements not met, do something
+//        return
+//    }
+//    
+//    // Do stuff with x
+//}
+//
+//func fooNonOptionalBad(x: Int) {
+//    if x <= 0 {
+//        // Value requirements not met, do something
+//        return
+//    }
+//    
+//    // Do stuff with x
+//}
