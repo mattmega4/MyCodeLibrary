8d358ebb19115de91438a55f5f6ae619c2347d8f
mattmega4 <mattmega4@gmail.com>
2018-03-27 21:36:30 -0700

    removing commit

---
+++ MyCodeLibrary/Remove Commit from Repo.swift
@@ -0,0 +1,13 @@
+//
+//  Remove Commit from Repo.swift
+//  MyCodeLibrary
+//
+//  Created by Matthew Howes Singleton on 3/12/18.
+//  Copyright © 2018 Matthew Singleton. All rights reserved.
+//
+
+import Foundation
+
+
+// https://help.github.com/articles/removing-sensitive-data-from-a-repository/
+
